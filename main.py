303d81a8666d2c8120dafff7723972845d9c0716
5-ter <5-ter@users.noreply.github.com>
2026-04-03 08:04:49 -0300

    added dir monitor, changed moving to copying, hash checking for corrupted copies, waiting for file download finish, and some minor stuff

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 from pathlib import Path
 import shutil
-import sys
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+import hashlib
+import os 
 
 
 
@@ -57,20 +61,51 @@ def get_file_hex(file_path, i):
             file_magic =file_bytes.hex().upper()
     return file_magic
 
-#handle renaming and moving files
+
+
+
+
+## check file hash for avoiding corruption
+def hash_file(file):
+    # open hash blender (md5 bc im cheap)
+    h = hashlib.md5()
+    #open the file, step through its bytes to add to the hash blender
+    with open(file, "rb") as f:
+        #lambda here steps the func, else it would return only the first chunk
+        # 8192 is just a good size here
+        for chunk in iter(lambda: f.read(8192), b""):
+            h.update(chunk)
+
+    #return the blessing from blender hash god
+    return h.hexdigest()
+
+  
+
+
+
+#handle renaming and hash checking before copying files
 def check_rename_move(file, path, i:int =0):
+    #separate name from path for checking duplicates
     name = file.name
     destination = path/name
     if destination.exists():
+        #nice recursion
         i += 1
         name = f"{file.stem}({i}){file.suffix}"
         check_rename_move(file, path, i)
     else:
-        shutil.move(file, destination)
+        shutil.copy2(file, destination)
+        #if hashes match delete original, and vice versa
+        if hash_file(file) == hash_file(destination):
+            os.remove(file)
+            print(f"Moved: {file} to {destination}")
+        else:
+            print(f"Failed to move {file} to {destination}")
+            os.remove(destination)
+
     
 
-
-def check_d_files():
+def sort_d_files():
 
     file_list = list(downloads.iterdir())
     for file in file_list:
@@ -87,7 +122,7 @@ def check_d_files():
                 if file_magic.startswith(key):
                     file_type = magic_dict[key]
                     break
-            #handle other cases
+            #handle cases dict didnt catch
             else:
                 file_magic = get_file_hex(file, 12)
                 if "66747970" in file_magic:
@@ -116,14 +151,58 @@ def check_d_files():
         except PermissionError:
             print(f"Permission error for {file}")
             continue
-        except:
-            print(f"failed to read or move {file}")
+        except Exception as e :
+            print(f"failed to read or move {file} : {e} ")
             continue
         
 
-check_d_folders()
-check_d_files()
+#wait for file to finish downloading
+def wait_for_file(file, interval =1):
+    #define previous size negative so it doesnt exit early
+    previous_size =-1
+    while True:
+        current_size= os.path.getsize(file)
+        if current_size == previous_size:
+            break
+        previous_size = current_size
+        time.sleep(interval)
 
+
+
+#define class for handling dir monitoring
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            return
+
+        wait_for_file(event.src_path)
+        sort_d_files()
+        return super().on_created(event)
+
+
+def main():
+    check_d_folders()
+    # instance these first 
+    obsrv = Observer()
+    handler = MyHandler()
+
+    obsrv.schedule(handler, downloads, recursive = False)
+    obsrv.start()
+
+    #sleep main thread untill exit program
+    try: 
+        while True:
+            time.sleep(1)
+    
+    except KeyboardInterrupt:
+        obsrv.stop()
+    obsrv.join()
+
+
+    
+
+if __name__ == "__main__":
+    main()
     
 
 
